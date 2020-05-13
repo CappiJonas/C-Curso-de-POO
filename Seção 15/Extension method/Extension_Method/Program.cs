c4dd208f15ae420a770717998e62cacbb32c4087
Jonas Cappi <cappijonas@gmail.com>
2020-05-13 09:38:25 -0300

    Meu estudo de hoje sobre Extension Methods

---
+++ Seção 15/Extension method/Extension_Method/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Extension_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2020, 5, 13, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
